4bf340e8fedb1c23def39f7d25895712fee7e491
Joseph Levy <josephlevy222@gmail.com>
2024-03-13 08:48:43 -0700

    Initial Commit

---
+++ ScientificNotation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScientificNotation
+//
+//  Created by Joseph Levy on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScientificNotation/ScientificNotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScientificNotationApp.swift
+//  ScientificNotation
+//
+//  Created by Joseph Levy on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ScientificNotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

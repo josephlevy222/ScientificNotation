dd85b249e08c68e4a7e73628de6341a292edaf85
Joseph Levy <josephlevy222@gmail.com>
2024-03-14 10:27:24 -0700

    Full code

--- ScientificNotation/ContentView.swift
+++ ScientificNotation/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+	let numbers: [Double] = {
+		var pi = -3.1415926535
+		var array = [pi/5e6]
+		for _ in 0...10 {
+			array = array + [-array.last!*50.0]
+		}
+		array = array + [13.54321567788, 0.5123456789, 0.0]
+		return array
+	}()
+	@State var format = "%#.4g"
     var body: some View {
-        VStack {
+		VStack(alignment: .leading) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+			ForEach(numbers, id: \.self) { number in
+				Text(String(scientific: number, format: format, exponentExclusion: 0..<3 )+"\t\t for \(String(format: format, number))").font(.title2)
+			}
+			TextField("Format", text: $format).border(.black)
         }
         .padding()
     }
--- ScientificNotation/ScientificNotation.swift
+++ ScientificNotation/ScientificNotation.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+func scientificNotation(format: String = "%g",_ x: Double, exponentExclusion: any RangeExpression<Int> = 0...1) -> String {
+	let exponent = (x == 0) ? 0 : Int(log10(abs(x)).rounded(.down))
+	if exponentExclusion.contains(exponent)  { return String(format: format, x).trimmingCharacters(in: [" "]) }
+	let mantissa =  x/pow(10.0, Double(exponent))
+	return String(format: format, mantissa).trimmingCharacters(in: [" "])
+	+ "×10" + (exponent < 0 ? "⁻" : "") + String(abs(exponent)).map(expDigits).joined()
+	
+	func expDigits(_ cDigit: Character) -> String {
+		["0":"⁰","1":"¹","2":"²","3":"³","4":"⁴","5":"⁵","6":"⁶","7":"⁷","8":"⁸","9":"⁹"][String(cDigit)] ?? ""
+	}
+}
+	
+extension String {
+	init(scientific: Double, format: String = "%g", exponentExclusion: any RangeExpression<Int> = 0...1) {
+		self = scientificNotation(format: format, scientific, exponentExclusion: exponentExclusion)
+	}
+}
